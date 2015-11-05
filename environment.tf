a498a44d9e38deb38e1029f562e50b38d254722c
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-11-05 13:12:13 +0200

    fix env config

--- environment.tf
+++ environment.tf
@@ -1,10 +1,10 @@
- provider "aws" {
-  2     access_key = "${var.aws_key}"
-  3     secret_key = "${var.aws_secret}" 
-  4     region = "eu-west-1"
-  5 }
-  6 
-  7 resource "aws_instance" "example" {
-  8     ami = "ami-bff32ccc"
-  9     instance_type = "t2.micro"
- 10 }
+provider "aws" {
+    access_key = "${var.aws_key}"
+    secret_key = "${var.aws_secret}" 
+    region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-bff32ccc"
+    instance_type = "t2.micro"
+}
